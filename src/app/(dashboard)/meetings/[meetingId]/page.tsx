3963dfa225af3f436ddac5055795cd45d0ef86f3
Daring Cλlf <daringcalf@outlook.com>
2025-06-29 16:32:14 +0800

    feat(meetings): implement meeting detail view with loading and error states, add update and delete functionality

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,5 +1,49 @@
-const MeetingDetailPage = () => {
-  return <div>TODO: Implement meeting detail view</div>;
+import { auth } from '@/lib/auth';
+import MeetingDetailView, {
+  MeetingDetailViewError,
+  MeetingDetailViewLoading,
+} from '@/modules/meetings/ui/views/meeting-detail-view';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+const MeetingDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ meetingId: string }>;
+}) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect('/sign-in');
+  }
+
+  const { meetingId } = await params;
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({
+      id: meetingId,
+    })
+  );
+
+  // TODO: prefetch meeting transcripts
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingDetailViewLoading />}>
+        <ErrorBoundary fallback={<MeetingDetailViewError />}>
+          <MeetingDetailView meetingId={meetingId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default MeetingDetailPage;
